97ad4eddf6c102484e5c4f47e9bf56186d3ae10a
David Peicho <david.peicho@gmail.com>
2021-12-23 20:02:25 +0100

    unity: migrate to latest 2021

--- unityapp/Assets/Scripts/AutoRotate.cs
+++ unityapp/Assets/Scripts/AutoRotate.cs
@@ -7,7 +7,6 @@ public class AutoRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color", new Color(1.0f, 0.0f, 0.0f));
         if (Input.touchCount == 0)
         {
             transform.Rotate(0.0f, Time.deltaTime * 50.0f, 0.0f);
